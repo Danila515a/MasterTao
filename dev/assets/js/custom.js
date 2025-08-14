2ad70f7732e0021e09fb8b77e18cdc541ef12ef9
DanilaZot93 <g9nchik@gmail.com>
2025-08-14 14:38:45 +0300

    styles  added styles ti banner slider

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,23 +1,23 @@
 const btnBurger = document.querySelector('.hamburger')
 btnBurger.addEventListener('click', ()=>btnBurger.classList.toggle('is-active') )
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
+const swiperBanner = new Swiper('.swiper-banner', {
+  
     direction:'horizontal',
     loop: true,
     
 
-  // If we need pagination
+  
     pagination: {
     clickable: true,
     el: '.swiper-pagination',
     },
 
-  // Navigation arrows
+  
     navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
     },
 
-  // And if we need scrollbar
+  
     
 });
