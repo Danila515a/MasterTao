54fe52a5c4d5dea3b1bb3b9d61d584606ad01dd6
DanilaZot93 <g9nchik@gmail.com>
2025-08-24 22:06:50 +0300

    feat:adoptation slider for mobile

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,23 +1,24 @@
 const btnBurger = document.querySelector('.hamburger')
 btnBurger.addEventListener('click', ()=>btnBurger.classList.toggle('is-active') )
 const swiperBanner = new Swiper('.swiper-banner', {
-  
+
     direction:'horizontal',
     loop: true,
     
 
-  
+
     pagination: {
     clickable: true,
     el: '.swiper-pagination',
     },
 
-  
+
     navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
     },
+    
 
-  
+
     
 });
