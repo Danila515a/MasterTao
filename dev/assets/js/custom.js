5742ce1d5036e76c53e6e15cc2d4068a9f18b008
DanilaZot93 <g9nchik@gmail.com>
2025-08-25 23:36:01 +0300

    feat:creat banner height feature

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -4,21 +4,29 @@ const swiperBanner = new Swiper('.swiper-banner', {
 
     direction:'horizontal',
     loop: true,
+    spaceBetween: 24,
     
-
-
-    pagination: {
-    clickable: true,
-    el: '.swiper-pagination',
-    },
-
-
     navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
     },
     
-
-
+    pagination: {
+    clickable: true,
+    el: '.swiper-pagination',
+    },
     
 });
+const header = document.querySelector('header');
+const banner = document.querySelector('.swiper-banner')
+
+
+const bannerVariable = banner.getAttribute('style')
+const headerHeight = header.offsetHeight
+
+
+console.log(header.offsetHeight)
+console.log(bannerVariable)
+
+banner.setAttribute('style','--header-height:' + headerHeight + 'px')
+
